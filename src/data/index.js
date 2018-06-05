31d67dd32e45dfbbae37d42d18884940b6293773
Alexander Fokin <fokin.aleksandr@otr.ru>
2018-06-05 07:32:07 +0300

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,44 @@
+const items = {
+  '1': {
+    id: '1',
+    img: './girl-1.png',
+    name: 'Платье-миди с расклешенной юбкой',
+    code: '82039-11',
+    size: '44',
+    color: 'синий',
+    quantity: 1,
+    price: 2450
+  },
+  '2': {
+    id: '2',
+    img: './shoe.png',
+    name: 'Туфли женские украшенные кружвными вставками',
+    code: '13524-01',
+    size: '38',
+    color: 'черный',
+    quantity: 1,
+    price: 2450
+  },
+  '3': {
+    id: '3',
+    img: './girl-2.png',
+    name: 'Платье-миди',
+    code: '75039',
+    size: '44',
+    color: 'белый',
+    quantity: 1,
+    price: 2450
+  },
+  '4': {
+    id: '4',
+    img: './girl-2.png',
+    name: 'Платье-миди',
+    code: '75039',
+    size: '44',
+    color: 'белый',
+    quantity: 1,
+    price: 2450
+  }
+};
+
+export default items;
